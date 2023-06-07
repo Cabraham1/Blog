4302215cd7027c8b4216fc62497bfbcf9acb3ffb
cabraham1 <christopherabraham8@gmail.com>
2023-06-07 21:46:26 +0100

    Create Like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_203150) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_204415) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "Title"
@@ -32,4 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_203150) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
 end
