d1a0489d705584d737e8fb7369da14ec22c11f59
cabraham1 <christopherabraham8@gmail.com>
2023-06-09 08:35:07 +0100

    post test

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,14 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_create :increment_user_posts_counter
 
   def increment_user_posts_counter
-    user.increment!(:posts_counter)
+    author.increment!(:posts_counter)
   end
 
   def recent_comments
