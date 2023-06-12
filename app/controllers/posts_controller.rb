5b61f2d31b1c8dc3a788f8feeb4f188a6860aa13
cabraham1 <christopherabraham8@gmail.com>
2023-06-12 17:08:37 +0100

    Update controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-PostsController < ApplicationController
+class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = Post.where(user_id: params[@user.id])
