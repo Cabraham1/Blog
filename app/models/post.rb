e3b009bd63d69a7bb3ec7bb658105682b2724bf3
cabraham1 <christopherabraham8@gmail.com>
2023-06-14 22:10:10 +0100

    Update comments in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,25 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments, foreign_key: :post_id
-  has_many :likes, foreign_key: :post_id
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  after_save :update_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_create :increment_user_posts_counter
+  def return_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 
-  def increment_user_posts_counter
+  private
+
+  def update_posts_counter
     author.increment!(:posts_counter)
   end
 
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
+  def update_likes_counter
+    post.increment!(:likes_counter)
   end
 end
