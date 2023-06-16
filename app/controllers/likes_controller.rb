6930e87fd3987fa864a75ec7d6fc80791fe92803
cabraham1 <christopherabraham8@gmail.com>
2023-06-16 10:50:17 +0100

    linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,9 +2,9 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(post_id: params[:post_id], user_id: current_user.id)
     begin
-      @like.save
+      @like.save!
       redirect_to user_post_path(params[:user_id], params[:post_id])
-    rescue NoMethodError => e
+    rescue ActiveRecord::RecordInvalid => e
       render :error, status: :unprocessable_entity
     end
   end
