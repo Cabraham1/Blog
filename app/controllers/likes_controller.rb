a29fadc86752867b7df59e2c77022b97970fff96
christopherabraham8@gmail.com <christopherabraham8@gmail.com>
2023-09-01 21:08:24 +0100

    add apis

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,3 @@
 class LikesController < ApplicationController
-  load_and_authorize_resource
-  def create
-    @like = current_user.likes.new
-    @like.post_id = params[:post_id]
-    if @like.save
-      redirect_to user_post_path(current_user, @like.post)
-    else
-      render :create
-    end
-  end
+  
 end
