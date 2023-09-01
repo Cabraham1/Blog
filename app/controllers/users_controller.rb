bf42c54be6b3400c79458461321babfe4f78e985
mrcbq <marcobaquero@gmail.com>
2023-09-01 15:15:46 -0500

    Update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
-  
+  def index
+    @users = User.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @users, status: 200 }
+    end
+  end
+  def show
+    if params[:id] == 'sign_out'
+      sign_out current_user
+      redirect_to new_user_session_path, notice: 'Sign out succesfully' unless @user
+    else
+      @user = User.find_by(id: params[:id])
+      respond_to do |format|
+        format.html { redirect_to users_path, notice: 'User found' unless @user }
+        format.json { render json: @user, status: 200 }
+      end
+    end
+  end
 end
